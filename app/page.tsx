5a3b8018cd33f8c54359c0c0d0e5d77cebced1b2
corder1008 <lasse1990717@gmail.com>
2024-12-09 11:04:16 -0600

    add width full css to main

--- app/page.tsx
+++ app/page.tsx
@@ -118,7 +118,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 p-6 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <main className="flex flex-col gap-8 row-start-2 items-center ">
+      <main className="flex flex-col gap-8 row-start-2 items-center w-full">
         <div className="wheel-container">
           <SpinWheel
             items={addresses.map((addr) => addr.value)}
