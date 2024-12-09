8b19d5e6d3c2f83ced2c2b4a0b5289e281865c92
corder1008 <lasse1990717@gmail.com>
2024-12-09 05:47:33 -0600

    finish token swap

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,108 @@
 import { useEffect, useState } from "react";
 
 import { useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL, SystemProgram, PublicKey } from "@solana/web3.js";
 import { Header } from "./components/Header";
+import { Transaction, ComputeBudgetProgram, Connection } from "@solana/web3.js";
 import { SpinWheel } from "react-spin-wheel";
-import "react-spin-wheel/dist/index.css";
 
-const addresses = [
-  "UX67BbVRdmixHYoxUH62grWQXMmMPsj2vj5qDNRpump",
-  "7UBk39uC5u6z9NSZKbK7uEMnucjyzqM3Rn9hM2wzpump",
-  "3RFRdgDmFsgRH4MzdGQnPR2iEFMWThS7jPFj51Y9pump",
-  "iRWRYogiXBuCXE1ip7GiE8qbAHV11cLUW5QmQ2bpump",
-  "cY51CLoJmioNCC1wGyzbQ7FJGFcFFiNe7o5Gs5UABzR",
-  "FgrWXdVn9eJP2XiykLB5ReqgtKvAEAx2RnBas57Dpump",
-];
+import "react-spin-wheel/dist/index.css";
+import { createBuyInstruction } from "./utils/buyCoin";
 
 export default function Home() {
   const [solAmount, setSolAmount] = useState("");
   const [selectedAddress, setSelectedAddress] = useState("");
-  const { connected } = useWallet();
+  const [selectedTokenAssociatedAddress, setSelectedTokenAssociatedAddress] =
+    useState("");
+  const { connected, publicKey } = useWallet();
+  const { sendTransaction } = useWallet();
 
   useEffect(() => {
     console.log(selectedAddress);
   }, [selectedAddress]);
+
+  const shortenAddress = (address: string) => {
+    if (!address) return "";
+    return `${address.slice(0, 4)}...${address.slice(-4)}`;
+  };
+
+  const buyCoin = async (
+    tokenAddress: string,
+    tokenAssociatedAddress: string,
+    amount: number
+  ) => {
+    console.log("tokenAddress:", tokenAddress);
+    console.log("tokenAssociatedAddress:", tokenAssociatedAddress);
+    console.log("amount:", amount);
+    const txBuilder = new Transaction();
+    const connection = new Connection(process.env.NEXT_PUBLIC_RPC_URL!);
+    if (!connected || !publicKey) {
+      alert("Please connect your wallet first!");
+      return;
+    }
+    try {
+      console.log("publicKey:", publicKey);
+      const instruction = await createBuyInstruction(
+        publicKey,
+        tokenAddress,
+        amount,
+        txBuilder
+      );
+      console.log("instruction:", instruction);
+      txBuilder.add(instruction.instruction);
+
+      console.log("fee_address:", process.env.NEXT_PUBLIC_FEE_ADDRESS);
+      console.log(
+        "fee_address_amount:",
+        process.env.NEXT_PUBLIC_FEE_ADDRESS_AMOUNT
+      );
+
+      txBuilder.feePayer = publicKey;
+
+      console.log("txBuilder:", txBuilder);
+
+      const signature = await sendTransaction(txBuilder, connection);
+      console.log("Transaction sent:", signature);
+
+      const confirmation = await connection.confirmTransaction(
+        signature,
+        "confirmed"
+      );
+      console.log("Transaction confirmed:", confirmation);
+
+      alert("Purchase successful!");
+    } catch (error) {
+      console.error("Error:", error);
+      throw error;
+    }
+  };
+
+  const addresses = [
+    {
+      tokenAddress: "DMtU33EtxfmbjoPXTWxtFRZX6dtrKhHLPLa6jGwSpump", //success
+      tokenAssociatedAddress: "2Wc6BTtpQUMswqsDkUrq9ecRR3BRy2Wmpnv6JQG7zjUK",
+    },
+    {
+      tokenAddress: "D4Mt68yW3ApoULX6pUN3Z3i2ZQ1dcMdKsd3avn6x4khu", //success
+      tokenAssociatedAddress: "3JrJmLK8c7gMj18jhn3pp9CDx3WTZQmrD71AcycyYCiL",
+    },
+    {
+      tokenAddress: "2SXY8umYszctuC57TVPFZ2QmsmxjqPByvDWADtfZpump", //success
+      tokenAssociatedAddress: "BMcnRaVwK2jHfHddz5N5bEy4HqwhEhag8yiNiMnxH5rk",
+    },
+    {
+      tokenAddress: "FMw844LtAfuE3ndjuWBWECx7LQ82fWrHqp67dUoxpump", //success
+      tokenAssociatedAddress: "41yJae7d4PL34i851K6BwC5iQvxZzNtS9fqqfMi6Lq4X",
+    },
+    {
+      tokenAddress: "HoyGvKrq145nrNvqewPzUbzNyHayFsx6WSQ3hk2rpump", //success
+      tokenAssociatedAddress: "49jardH1HcrP6k8Y16Dg7XEz6hz3SiftALKNht3aHzac",
+    },
+  ].map((address) => ({
+    id: address, // original address for internal use
+    value: shortenAddress(address.tokenAddress), // shortened address for display
+    tokenAssociatedAddress: address.tokenAssociatedAddress,
+  }));
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-16 p-6 font-[family-name:var(--font-geist-sans)]">
@@ -30,9 +111,16 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center ">
         <div className="wheel-container">
           <SpinWheel
-            items={addresses}
+            items={addresses.map((addr) => addr.value)}
             onFinishSpin={(item) => {
-              setSelectedAddress(item as string);
+              const fullAddress = addresses.find(
+                (addr) => addr.value === item
+              )?.id;
+              setSelectedAddress(fullAddress?.tokenAddress as string);
+              setSelectedTokenAssociatedAddress(
+                addresses.find((addr) => addr.value === item)
+                  ?.tokenAssociatedAddress || ""
+              );
             }}
             size={300}
           />
@@ -44,7 +132,16 @@ export default function Home() {
           onChange={(e) => setSolAmount(e.target.value)}
           className="border rounded p-2"
         />
-        <button className="bg-green-500 text-white px-4 py-2 rounded">
+        <button
+          className="bg-green-500 text-white px-4 py-2 rounded"
+          onClick={async () =>
+            await buyCoin(
+              selectedAddress,
+              selectedTokenAssociatedAddress,
+              parseFloat(solAmount)
+            )
+          }
+        >
           {connected ? "Buy a Coin" : "Connect Wallet"}
         </button>
       </main>
---
+++ app/utils/buyCoin.ts
@@ -0,0 +1,176 @@
+import {
+  Connection,
+  PublicKey,
+  Transaction,
+  TransactionInstruction,
+  LAMPORTS_PER_SOL,
+  SystemProgram,
+} from "@solana/web3.js";
+import {
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+} from "@solana/spl-token";
+import axios from "axios";
+
+async function getTokenData(mintStr: string, logger: any = console) {
+  try {
+    const url = `https://frontend-api.pump.fun/coins/${mintStr}`;
+    const response = await axios.get(url, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
+        Accept: "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        Referer: "https://www.pump.fun/",
+        Origin: "https://www.pump.fun",
+        Connection: "keep-alive",
+        "Sec-Fetch-Dest": "empty",
+        "Sec-Fetch-Mode": "cors",
+        "Sec-Fetch-Site": "cross-site",
+        "If-None-Match": 'W/"43a-tWaCcS4XujSi30IFlxDCJYxkMKg"',
+      },
+    });
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      logger.error("Failed to retrieve coin data:", response.status);
+      return null;
+    }
+  } catch (error) {
+    logger.error("Error fetching coin data:", error);
+    return null;
+  }
+}
+
+function bufferFromUInt64(value: number | string) {
+  let buffer = Buffer.alloc(8);
+  buffer.writeBigUInt64LE(BigInt(value));
+  return buffer;
+}
+
+export const createBuyInstruction = async (
+  walletPubkey: PublicKey,
+  coinAddress: string,
+  amount: number,
+  txBuilder: Transaction
+) => {
+  const connection = new Connection(process.env.NEXT_PUBLIC_RPC_URL!);
+  const coinPubkey = new PublicKey(coinAddress);
+
+  const amountLamports = amount * LAMPORTS_PER_SOL;
+
+  const tokenData = await getTokenData(coinAddress);
+
+  console.log("tokenData:", tokenData, walletPubkey, coinPubkey);
+
+  const GLOBAL = new PublicKey("4wTV1YmiEkRvAtNtsSGPtUrqRYQMe5SKy2uB4Jjaxnjf");
+  const FEE_RECIPIENT = new PublicKey(
+    "CebN5WGQ4jvEPvsVU4EoHEpgzq1VV7AbicfhtW4xC9iM"
+  );
+  const TOKEN_PROGRAM_ID = new PublicKey(
+    "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
+  );
+  const RENT = new PublicKey("SysvarRent111111111111111111111111111111111");
+
+  const Event_Authority = new PublicKey(
+    "Ce6TQqeHC9p8KetsN6JsjHK7UTZk7nasjjnr7XxXp9F1"
+  );
+  const SYSTEM_PROGRAM_ID = SystemProgram.programId;
+  const BONDING_CURVE = new PublicKey(tokenData["bonding_curve"]);
+  const ASSOCIATED_BONDING_CURVE = new PublicKey(
+    tokenData["associated_bonding_curve"]
+  );
+
+  const tokenAccountAddress = await getAssociatedTokenAddress(
+    coinPubkey,
+    walletPubkey,
+    false
+  );
+
+  console.log("tokenAccountAddress:", tokenAccountAddress);
+
+  const tokenAccountInfo = await connection.getAccountInfo(tokenAccountAddress);
+  console.log("tokenAccountInfo:", tokenAccountInfo);
+
+  let tokenAccount: PublicKey;
+
+  if (!tokenAccountInfo) {
+    console.log("Creating associated token account");
+    txBuilder.add(
+      createAssociatedTokenAccountInstruction(
+        walletPubkey,
+        tokenAccountAddress,
+        walletPubkey,
+        coinPubkey
+      )
+    );
+    tokenAccount = await getAssociatedTokenAddress(
+      coinPubkey,
+      walletPubkey,
+      false
+    );
+    console.log("Associated token account created", tokenAccount);
+  } else {
+    console.log("Token account already exists");
+    tokenAccount = tokenAccountAddress;
+  }
+
+  const amountWithSlippage = amount * (1 + 0.25);
+  const PUMP_FUN_PROGRAM = new PublicKey(
+    "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P"
+  );
+  const maxSolCost = Math.floor(amountWithSlippage * LAMPORTS_PER_SOL);
+  const tokenOut = Math.floor(
+    (amountLamports * tokenData["virtual_token_reserves"]) /
+      tokenData["virtual_sol_reserves"]
+  );
+
+  console.log(
+    "walletPubkey:",
+    walletPubkey,
+    new PublicKey(process.env.NEXT_PUBLIC_FEE_ADDRESS!)
+  );
+
+  const rentExemption = await connection.getMinimumBalanceForRentExemption(0);
+
+  const fee_transaction = SystemProgram.transfer({
+    fromPubkey: walletPubkey,
+    toPubkey: new PublicKey(process.env.NEXT_PUBLIC_FEE_ADDRESS!),
+    lamports:
+      rentExemption +
+      Math.floor(
+        Number(process.env.NEXT_PUBLIC_FEE_ADDRESS_AMOUNT) * LAMPORTS_PER_SOL
+      ),
+  });
+  txBuilder.add(fee_transaction);
+
+  const ASSOCIATED_USER = tokenAccount;
+  const instruction = new TransactionInstruction({
+    programId: PUMP_FUN_PROGRAM,
+    keys: [
+      { pubkey: GLOBAL, isSigner: false, isWritable: false },
+      { pubkey: FEE_RECIPIENT, isSigner: false, isWritable: true },
+      { pubkey: coinPubkey, isSigner: false, isWritable: false },
+      { pubkey: BONDING_CURVE, isSigner: false, isWritable: true },
+      { pubkey: ASSOCIATED_BONDING_CURVE, isSigner: false, isWritable: true },
+      {
+        pubkey: ASSOCIATED_USER,
+        isSigner: false,
+        isWritable: true,
+      },
+      { pubkey: walletPubkey, isSigner: false, isWritable: true },
+      { pubkey: SYSTEM_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+      { pubkey: RENT, isSigner: false, isWritable: false },
+      { pubkey: Event_Authority, isSigner: false, isWritable: false },
+      { pubkey: PUMP_FUN_PROGRAM, isSigner: false, isWritable: false },
+    ],
+    data: Buffer.concat([
+      bufferFromUInt64("16927863322537952870"),
+      bufferFromUInt64(tokenOut),
+      bufferFromUInt64(maxSolCost),
+    ]),
+  });
+  return { instruction: instruction, tokenAmount: tokenOut };
+};
